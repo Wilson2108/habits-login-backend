7e59024f74fc2a4c9b220ae081b9dcf3dfa7d8d9
Wilson Linares Arley Isaac <awlinare@tlmx.mx>
2026-03-12 10:57:08 -0600

    login_logs

--- database.py
+++ database.py
@@ -8,3 +8,4 @@ client = MongoClient(os.getenv("MONGO_URI"))
 db = client[os.getenv("MONGO_DB_NAME")]
 
 users_collection = db["users"]
+login_logs_collection = db["login_logs"]
--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, HTTPException
-from database import users_collection
+from database import users_collection, login_logs_collection
 from models import UserRegister, UserLogin
 from auth import hash_password, verify_password, create_access_token
+from datetime import datetime
+from fastapi import Request
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -24,16 +26,56 @@ def register(user: UserRegister):
 
     return {"message": "User created successfully"}
 
+# @router.post("/login")
+# def login(user: UserLogin):
+#     db_user = users_collection.find_one({"email": user.email})
+
+#     if not db_user:
+#         raise HTTPException(status_code=401, detail="Invalid credentials")
+
+#     if not verify_password(user.password, db_user["password"]):
+#         raise HTTPException(status_code=401, detail="Invalid credentials")
+
+#     token = create_access_token({"sub": user.email})
+
+#     return {"access_token": token, "token_type": "bearer"}
 @router.post("/login")
-def login(user: UserLogin):
+def login(user: UserLogin, request: Request):
+
+    client_ip = request.client.host
+
     db_user = users_collection.find_one({"email": user.email})
 
     if not db_user:
+        login_logs_collection.insert_one({
+            "email": user.email,
+            "timestamp": datetime.utcnow(),
+            "status": "failed",
+            "reason": "user_not_found",
+            "ip": client_ip
+        })
+
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
     if not verify_password(user.password, db_user["password"]):
+
+        login_logs_collection.insert_one({
+            "email": user.email,
+            "timestamp": datetime.utcnow(),
+            "status": "failed",
+            "reason": "wrong_password",
+            "ip": client_ip
+        })
+
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
     token = create_access_token({"sub": user.email})
+
+    login_logs_collection.insert_one({
+        "email": user.email,
+        "timestamp": datetime.utcnow(),
+        "status": "success",
+        "ip": client_ip
+    })
 
     return {"access_token": token, "token_type": "bearer"}
